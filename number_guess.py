585d20ab54530afb31ddede0ff0fe358bc0a741d
IMMYIMRAN85 <imran385@outlook.com>
2025-11-14 01:00:26 +0000

    Revert "Merge pull request #2 from IMMYIMRAN85/feature-attempt-counter"
    This reverts commit dd294773cb8f2ec516ffacadb50f413b3a807c14, reversing
    changes made to 7cda988dd26f7b29bcc5cc0b59b54c6efa47afaa.

--- number_guess.py
+++ number_guess.py
@@ -3,17 +3,19 @@ import random
 def main():
     print("Welcome to Guess the Number!")
     secret = random.randint(1, 10)
-    attempts = 0
 
-    while True:
-        attempts += 1
-        guess = int(input("Guess a number between 1 and 10: "))
+    user_input = input("Guess a number between 1 and 10: ")
 
-        if guess == secret:
-            print(f"You guessed it in {attempts} attempt(s)!")
-            break
-        else:
-            print("Nope, try again.")
+    try:
+        guess = int(user_input)
+    except ValueError:
+        print("That wasn't a valid number. Please run the game again and enter a number.")
+        return
+
+    if guess == secret:
+        print("You guessed it!")
+    else:
+        print("Sorry, the number was", secret)
 
 if __name__ == "__main__":
     main()
