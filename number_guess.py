b97bdb15d4d3f94c94a332fcf899f21668542d86
IMMYIMRAN85 <imran385@outlook.com>
2025-11-14 00:11:50 +0000

    Add input validation

--- number_guess.py
+++ number_guess.py
@@ -4,7 +4,13 @@ def main():
     print("Welcome to Guess the Number!")
     secret = random.randint(1, 10)
 
-    guess = int(input("Guess a number between 1 and 10: "))
+    user_input = input("Guess a number between 1 and 10: ")
+
+    try:
+        guess = int(user_input)
+    except ValueError:
+        print("That wasn't a valid number. Please run the game again and enter a number.")
+        return
 
     if guess == secret:
         print("You guessed it!")
@@ -13,3 +19,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
