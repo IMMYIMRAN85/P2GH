8a6ed9cd43a1ed8fd1f8e10a64b3a0b4ba54e170
IMMYIMRAN85 <imran385@outlook.com>
2025-11-14 00:25:19 +0000

    Use simple number_guess.py version from feature-input-validation

--- number_guess.py
+++ number_guess.py
@@ -4,13 +4,7 @@ def main():
     print("Welcome to Guess the Number!")
     secret = random.randint(1, 10)
 
-    user_input = input("Guess a number between 1 and 10: ")
-
-    try:
-        guess = int(user_input)
-    except ValueError:
-        print("That wasn't a valid number. Please run the game again and enter a number.")
-        return
+    guess = int(input("Guess a number between 1 and 10: "))
 
     if guess == secret:
         print("You guessed it!")
@@ -19,4 +13,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
